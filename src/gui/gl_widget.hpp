6cac97d859948de152bffa218a8a5f73c3f76f67
nagato0614 <nagato0614@gmail.com>
2019-09-23 19:05:43 +0900

    [update]glwidgetの定義と宣言を分割

--- src/gui/gl_widget.hpp
+++ src/gui/gl_widget.hpp
@@ -7,7 +7,6 @@
 
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QOpenGLWidget>
-#include <QOpenGLFunctions>
 #include <QtOpenGL/QGL>
 #include <QtOpenGL/QGLWidget>
 #include <QImage>
@@ -17,44 +16,20 @@ class GLWidget : public QOpenGLWidget {
  Q_OBJECT
 
  public:
-	explicit GLWidget(QWidget *parent = nullptr) : QOpenGLWidget(parent) {}
+	explicit GLWidget(QWidget *parent = nullptr);
 
-	void SetImage(const QImage &image) {
-		image_ = image;
-		update();
-	}
+	void SetImage(const QImage &image);
 
  protected:
 
 	//  OpenGL 初期化
-	void initializeGL() override {
-		int width = geometry().width();
-		int height = geometry().height();
-		glViewport( 0, 0, width, height );
-		glMatrixMode(GL_PROJECTION);
-		glOrtho( -width * 0.5, width * 0.5, -height * 0.5, height * 0.5, -1.0, 1.0 );
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	}
+	void initializeGL() override;
 
 	//  ウィジットリサイズ時のハンドラ
-	void resizeGL(int width, int height) override {
-		//  ビューポートの指定
-		glViewport( 0, 0, width, height );
-	}
+	void resizeGL(int width, int height) override;
 
 	//  描画処理
-	void paintGL() override {
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		QImage gl_image = QGLWidget::convertToGLFormat(image_);
-		glDrawPixels(image_.width(),
-								 image_.height(),
-								 GL_RGBA,
-								 GL_UNSIGNED_BYTE,
-								 gl_image.bits());
-	}
+	void paintGL() override;
 
  private:
 	QImage image_;
